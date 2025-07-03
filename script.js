d0b165c17a8528ead97c496d6980e5ba397f529c
Erick Eduardo Souza Araujo <ericksouza23135@gmail.com>
2025-07-03 12:31:29 -0300

    slides criados

--- script.js
+++ script.js
@@ -96,13 +96,174 @@ const swiper3 = new Swiper('.mySwiper3', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-
-    effect:'cube',
-    keyboard: true,
-
+    
     mousewheel: {
         invert: true,
     },
+
+    effect:'cube',
+    keyboard: true,
+    grabCursor: true,
+});
+
+const effectb = document.getElementById('effectb')
+const swiperss = document.querySelectorAll('.carmud')
+const veteffect = ['fade', 'coverflow', 'flip', 'creative', 'cards', 'slide']
+
+effectb.addEventListener('mousedown', () => {
+    effectb.classList.add('muda')
 })
 
-const swiper3// definir o terceiro coursel como efeito dinamica ao clicar muda o efffect para aleatoria mudando o slide ou algo que funcione
+effectb.addEventListener('mouseup', () => {
+    effectb.classList.remove('muda')
+})
+
+effectb.addEventListener('mouseleave', () => {
+    effectb.classList.remove('muda')
+})
+
+let norepet = -1
+
+effectb.addEventListener('click', () => {
+    let ale 
+    do {
+        
+        ale = Math.floor(Math.random() * veteffect.length)
+    } while (ale === norepet);
+    norepet = ale
+
+    calc(veteffect[ale])
+
+    effectb.classList.add('muda')
+    setTimeout(() => {
+        effectb.classList.remove('muda')
+    }, 500);
+})
+
+let swiper5
+let swiper6
+let swiper7
+let swiper8
+let swiper9
+
+function calc(param) {
+    swiperss.forEach(swip => {
+        swip.style.display = 'none'
+    })
+
+    switch(param) {
+        case 'flip':
+            swiperss[3].style.display = 'block' 
+            if (!swiper5) {
+                swiper5 = new Swiper('.mySwiperFlip', {// para adicionar efeitos o swiper precisa estar visivel ou seja display none\
+                        
+                    navigation: {
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
+                    },
+
+                    pagination: {
+                        el:'.swiper-pagination'
+                    },
+
+                    effect:'flip',
+                    slidesPerView: 3,
+                })
+            }
+            break
+        case 'slide': 
+            swiperss[0].style.display = 'block'
+
+            break
+        case 'coverflow':
+            swiperss[1].style.display = 'block'
+            if (!swiper6) {
+                swiper6 = new Swiper('.mySwiperCoverflow', {
+                    effect: 'coverflow',
+                    spaceBetween: 40,
+                    coverflowEffect: {
+                        rotate: 40,
+                    }
+                })
+            }
+            break
+        case 'fade': 
+            swiperss[2].style.display = 'block'
+            if (!swiper7) {
+                swiper7 = new Swiper('.mySwiperFade', {
+                    pagination:{
+                        el:'.swiper-pagination',
+                    },
+
+                    navigation: {
+                        nextEl:'.swiper-button-next',
+                        prevEl:'.swiper-button-prev',
+                    },
+                    
+                    effect:'fade',
+                })
+            }
+            
+            break
+        case 'creative':
+            swiperss[4].style.display = 'block'
+            if (!swiper9) {
+                swiper9 = new Swiper('.mySwiperCreative', {
+                    effect: 'creative',
+                    creativeEffect: {
+                        prev: {
+                            shadow: true,
+                            translate:['-125%', 0, -800],
+                            rotate:[0, 0, -90],
+                        },
+
+                        next: {
+                            shadow: true,
+                            translate:['125%', 0, -800],
+                            rotate: [0, 0, 90],
+                        },
+                    }
+                })
+            }
+            break
+        case 'cards':
+            swiperss[5].style.display = 'block'
+            if (!swiper8) {
+                swiper8 = new Swiper('.mySwiperCards', {
+                    navigation: {
+                        nextEl:'.swiper-button-next',
+                        prevEl:'.swiper-button-prev',
+                    },
+                
+                    effect:'cards',
+                    touchRatio: 3.5,
+                })
+            }
+            break
+        default:
+            window.alert('[ERROR]')
+    }
+
+}
+
+const swiper4 = new Swiper('.mySwiperSlide', {    
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'progressbar',
+        dynamicBullets: false,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    
+    mousewheel: {
+        invert: true,
+    },
+
+    grabCursor: true,
+    resistanceRatio: 0,
+    keyPress: true,
+    spaceBetween: 30,
+})
